8ec84f557d1c40374d430684af195243e00bcac7
Gonzalo Martinez <gon.martinez@icloud.com>
2021-07-22 15:10:55 -0300

    Finalizado proceso de build

--- Assets/Editor/BuildProject.cs
+++ Assets/Editor/BuildProject.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 using UnityEditor;
 
 public class BuildProject : MonoBehaviour{
+    public static string version = System.Environment.GetEnvironmentVariable("projectVersion");
+    public static string gameName = System.Environment.GetEnvironmentVariable("gameName");
 
-    public static void BuildWindows()
+    public static void BuildWindows64()
     {
-
+        string buildPath = ".\\build\\" + version + "\\Win64\\" + gameName + ".exe";
         BuildPipeline.BuildPlayer(
             new string[] { "Assets\\Scenes\\Gameplay\\GamePlay.unity" },
-            ".\\build\\Juego.exe",
+            buildPath,
             BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+    }
+    public static void BuildWindows32()
+    {
+        string buildPath = ".\\build\\" + version + "\\Win32\\" + gameName + ".exe";
+        BuildPipeline.BuildPlayer(
+            new string[] { "Assets\\Scenes\\Gameplay\\GamePlay.unity" },
+            buildPath,
+            BuildTarget.StandaloneWindows,
             BuildOptions.None
         );
     }
--- build.ps1
+++ build.ps1
@@ -1,2 +1,32 @@
+$dateTime = (Get-Date).ToString('yyyyMMddhhmmss')
+$projectPath=".\"
+$buildMethodWin64="BuildProject.BuildWindows64"
+$buildMethodWin32="BuildProject.BuildWindows32"
+$env:gameName="Plataformote"
+$env:projectVersion="0.1_$dateTime"
+$env:environment="Test"
 
-Start-Process 'C:\Program Files\Unity\Hub\Editor\2020.3.14f1\Editor\Unity.exe' -ArgumentList " -projectPath .\ -executeMethod BuildProject.BuildWindows"
+echo "Project Path: $projectPath"
+echo "Project environment: $env:environment"
+echo "Project version: $env:projectVersion"
+echo ""
+echo "Building Windows 64 bit executable, Using build method: $buildMethodWin64"
+
+$resultado64 = Start-Process -Wait 'C:\Program Files\Unity\Hub\Editor\2020.3.14f1\Editor\Unity.exe' -ArgumentList "-nographics -batchmode -projectPath $buildPath -executeMethod $buildMethodWin64 -quit"
+
+
+echo "Building Windows 32 bit executable, Using build method: $buildMethodWin32"
+
+$resultado32 = Start-Process -Wait 'C:\Program Files\Unity\Hub\Editor\2020.3.14f1\Editor\Unity.exe' -ArgumentList "-nographics -batchmode -projectPath $buildPath -executeMethod $buildMethodWin32 -quit"
+
+if($resultado32 -ne 0){
+   exit 1
+}
+   
+if($resultado64 -ne 0){
+  exit 1
+}
+
+exit 0
+
+Read-Host -Prompt 'Proceso terminado'
